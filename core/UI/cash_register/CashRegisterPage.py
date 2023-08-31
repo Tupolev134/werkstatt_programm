b7dfe41d12489bd8488b73a7a30ca8373db4dbd2
The Overlanders <office@theoverlanders.de>
2023-08-31 09:54:38 +0200

    table reads in

--- core/UI/cash_register/CashRegisterPage.py
+++ core/UI/cash_register/CashRegisterPage.py
@@ -28,6 +28,7 @@ class CashRegisterPage(QMainWindow):
         rect = screen.availableGeometry()
         self.setMaximumHeight(rect.height())
         self.main_layout = QVBoxLayout(central_widget)
+        self.resize(600,900)
 
         # ------------------ create and add NavigationBar ------------------
         self.window_manager = window_manager
@@ -37,21 +38,14 @@ class CashRegisterPage(QMainWindow):
 
         # ------------------ create Expense List ------------------
         self.create_expense_list_section()
+        self.create_insert_section()
         self.register_data = CashRegisterData(REGISTER_PATH)
-
-        print(REGISTER_PATH)
+        self.populate_expense_table()
 
     def create_expense_list_section(self):
         self.expense_table = QTableWidget(self)
         self.expense_table.setColumnCount(4)  # Date, Name, Expense Type, Amount
         self.expense_table.setHorizontalHeaderLabels(["Date", "Name", "Expense Type", "Amount"])
-
-        # For the sake of demonstration, let's add a dummy row:
-        self.expense_table.insertRow(0)
-        self.expense_table.setItem(0, 0, QTableWidgetItem("2023-08-30"))
-        self.expense_table.setItem(0, 1, QTableWidgetItem("John Doe"))
-        self.expense_table.setItem(0, 2, QTableWidgetItem("Lunch"))
-        self.expense_table.setItem(0, 3, QTableWidgetItem("$10.50"))
 
         self.main_layout.addWidget(self.expense_table)
 
@@ -81,7 +75,7 @@ class CashRegisterPage(QMainWindow):
 
         # Name selection widget (QComboBox with hardcoded names)
         self.name_combobox = QComboBox(self)
-        self.name_combobox.addItems(['John Doe', 'Jane Smith', 'Alice', 'Bob'])  # Hardcoded names
+        self.name_combobox.addItems(['Felix Tappert', 'Szymon Goral', 'Steve Knauff', 'Leonardo Smura', 'Jonas Glöckner'])  # Hardcoded names
         input_layout.addWidget(QLabel("Name:"))
         input_layout.addWidget(self.name_combobox)
 
@@ -121,3 +115,4 @@ class CashRegisterPage(QMainWindow):
         # Optionally: clear the input fields
         self.expense_type_edit.clear()
         self.amount_edit.clear()
+        self.register_data.save_data()
